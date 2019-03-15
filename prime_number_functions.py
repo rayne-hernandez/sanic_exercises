84f9c6069ffc399499117b67c1432f0fa9a2985c
Rayne Hernandez <rayne@kountable.com>
2019-03-15 16:04:32 -0700

    Added async endpoint

--- prime_number_functions.py
+++ prime_number_functions.py
@@ -1,3 +1,5 @@
+import asyncio
+
 def is_prime_helper(n, previous_primes):
     for prime in previous_primes:
         if n % prime == 0:
@@ -8,6 +10,62 @@ def first_n_primes(n):
     if n == 1:
         return [2]
     primes = first_n_primes(n - 1)
+    next_prime = max(primes) + 1
+    while True:
+        if is_prime_helper(next_prime, primes):
+            primes.append(next_prime)
+            return primes
+        next_prime += 1
+
+async def async_first_n_primes_1(n):
+    if n == 1:
+        return [2]
+    primes = await async_first_n_primes_2(n - 1)
+    next_prime = max(primes) + 1
+    while True:
+        if is_prime_helper(next_prime, primes):
+            primes.append(next_prime)
+            print(primes)
+            return primes
+        next_prime += 1
+
+async def async_first_n_primes_2(n):
+    if n == 1:
+        return [2]
+    primes = await async_first_n_primes_3(n - 1)
+    next_prime = max(primes) + 1
+    while True:
+        if is_prime_helper(next_prime, primes):
+            primes.append(next_prime)
+            return primes
+        next_prime += 1
+
+async def async_first_n_primes_3(n):
+    if n == 1:
+        return [2]
+    primes = await async_first_n_primes_4(n - 1)
+    next_prime = max(primes) + 1
+    while True:
+        if is_prime_helper(next_prime, primes):
+            primes.append(next_prime)
+            return primes
+        next_prime += 1
+
+async def async_first_n_primes_4(n):
+    if n == 1:
+        return [2]
+    primes = await async_first_n_primes_5(n - 1)
+    next_prime = max(primes) + 1
+    while True:
+        if is_prime_helper(next_prime, primes):
+            primes.append(next_prime)
+            return primes
+        next_prime += 1
+
+async def async_first_n_primes_5(n):
+    if n == 1:
+        return [2]
+    primes = await async_first_n_primes_1(n - 1)
     next_prime = max(primes) + 1
     while True:
         if is_prime_helper(next_prime, primes):
--- prime_numbers_api.py
+++ prime_numbers_api.py
@@ -4,15 +4,21 @@ from sanic.response import json
 
 app = Sanic()
 
-@app.route("/"  )
+@app.route("/")
 async def test(request):
     return json({"hello": "world"})
 
 @app.route("/first_n_primes")
-def primes_handler(request):
+async def primes_handler(request):
     n = int(request.args["n"][0])
     primes = pf.first_n_primes(n)
     return json({"n": n, "primes": primes})
+
+@app.route("/async_first_n_primes")
+async def async_prime_handler(request):
+    n = int(request.args["n"][0])
+    primes = await pf.async_first_n_primes_1(n)
+    return json({"n":n, "primes":primes})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
